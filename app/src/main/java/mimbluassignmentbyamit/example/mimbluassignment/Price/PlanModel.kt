7fcff9e971fbcf9eaeafd817069ff3f0c607ff25
maityamit <maityamit308@gmail.com>
2023-08-14 00:25:24 +0530

    little_change

--- app/src/main/java/mimbluassignmentbyamit/example/mimbluassignment/Price/PlanModel.kt
+++ app/src/main/java/mimbluassignmentbyamit/example/mimbluassignment/Price/PlanModel.kt
@@ -5,3 +5,12 @@ data class PlanModel(
     val copyrighths: String,
     val list: ArrayList<PlanListModel>
 )
+
+class PlanListModel(
+    var duration: String,
+    var description: String,
+    var video_description: String,
+    var currency_code: String,
+    var total_price: String,
+    var discounted_price: String
+)
